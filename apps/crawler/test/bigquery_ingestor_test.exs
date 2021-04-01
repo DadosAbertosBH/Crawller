59907373aced25ded7e9b36c5be812dcb7aa126e
Guilherme Castro <guilhermecastro@positivosolucoes.com.br>
2021-04-01 11:07:39 -0300

    assert json field

--- apps/crawler/test/bigquery_ingestor_test.exs
+++ apps/crawler/test/bigquery_ingestor_test.exs
@@ -2,16 +2,28 @@ defmodule BigQueryIngestorTest do
   use ExUnit.Case, async: true
   use Assertions.Case
 
-  test "when decode with success returns {:ok, [{id, %{NumeroLinha, Linha, Nome}}]" do
+  test "assert send requests with success" do
     bypass = Bypass.open()
 
     Application.put_env(:google_api_big_query, :base_url, "http://localhost:#{bypass.port}/")
 
     Bypass.expect(bypass, fn conn ->
-      IO.inspect(conn)
+      {:ok, body, _} = Plug.Conn.read_body(conn)
+      [rows] = Poison.decode!(body)["rows"]
+      json = rows["json"]
 
-      assert conn.request_path ==
-               "/bigquery/v2/projects/dadosabertosdebh/datasets/dadosabertosdebh/tables/coordenadas_onibus/insertAll"
+      assert json["velocidade_instantanea"] == 0.0
+      assert json["codigo_do_veiculo"] == "40867"
+      assert json["codigo_evento"] == "105"
+      assert json["codigo_linha"] == "357"
+      assert json["coordenadas"] == "{\"type\":\"Point\",\"coordinates\":[-19.868567,-44.014435]}"
+      assert json["direcao_do_veiculo"] == "0"
+      assert json["distancia_pecorrida"] == 708.0
+      assert json["nome_linha"] == "ZOOLOGICO VIA SERRANO"
+      assert json["numero_linha"] == "4403A-01"
+      assert json["sentindo_da_viagem"] == "0"
+      assert json["timestamp"] == "2021-03-23T16:20:12"
+      assert json["velocidade_instantanea"] == 0.0
 
       Plug.Conn.resp(conn, 200, "{}")
     end)
